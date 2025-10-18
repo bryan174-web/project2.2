d85a73195a74e4578921dab62243bfdd3b35be7a
Bryan Sevilla <bsevilla799@insite.4cd.edu>
2025-10-18 13:36:27 -0700

    update. 10/18/25

--- js/script.js
+++ js/script.js
@@ -2,6 +2,21 @@
 date:9/28/2025
 assignment: JavaScript for Project 2: Photo Gallery */
 
+
+/*This glitches, then It starts to work the way I want it after all the images have ben showned */
+function swapImages(){
+  var $active = $('#myGallery .active');
+   var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
+  $active.fadeOut(function(){
+    $active.removeClass('active');
+    $next.fadeIn().addClass('active');
+  });
+}
+
+// Run our swapImages() function every 3secs
+setInterval('swapImages()', 3000);
+
+/* This button works*/
 var i = 0;
 var names = ['images/turtle2.jpg','images/turtle3.jpg','images/turtle4.jpg','images/turtle5.jpg'
   ,'images/turtle6.jpg','images/turtle7.jpg' , 'images/turtle8.jpg' , 'images/turtle9.jpg' ,'images/turtle10.jpg'
